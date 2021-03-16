e80179a999fbdc843fcbc75b500a2fd8d379ad8e
eliasneri <eliasneri@hotmail.com>
2021-03-16 16:30:59 -0300

    Implementando usando Generics

--- src/Program.java
+++ src/Program.java
@@ -7,21 +7,23 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		PrintService print = new PrintService();
+		PrintService<Integer> print = new PrintService<>();
 		
 		System.out.print("how many values? ");
 		int op = sc.nextInt();
 		
 			for (int i=1; i<=op; i++) {
-				int value = sc.nextInt();
+				Integer value = sc.nextInt();
 				print.addValue(value);
 				
 			}
 		
 		System.out.println();
 		print.print();
+		Integer x = print.first();
+		
 		System.out.println();
-		System.out.println("First: " + print.first());
+		System.out.println("First: " + x);
 		
 		sc.close();
 
